97fb5cf4e26490b8aa89a63a49325c3715537b6a
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-13 00:30:16 -0400

    added the meanTypes interface

---
+++ advanced-react/src/types/mealTypes.ts
@@ -0,0 +1,12 @@
+export interface Meal {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+}
+
+export interface Category {
+  idCategory: string;
+  strCategory: string;
+  strCategoryThumb: string;
+  strCategoryDescription: string;
+}
