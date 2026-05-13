3210d21db9ab86fa365226e43a81f8adc35da9b2
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-13 00:43:11 -0400

    Worked on Components file and home

---
+++ advanced-react/src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  message: string;
+}
+
+const ErrorMessage = ({ message }: Props) => {
+  return <h2>{message}</h2>;
+};
+
+export default ErrorMessage;
--- advanced-react/src/pages/Home.tsx
+++ advanced-react/src/pages/Home.tsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import Spinner from "../components/Spinner";
+import ErrorMessage from "../components/ErrorMessage";
+import type { Category } from "../types/mealTypes";
 
+interface CategoriesResponse {
+  categories: Category[];
+}
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const { data, loading, error } =
+    useFetch<CategoriesResponse>(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+
+  if (loading) return <Spinner />;
+
+  if (error) {
+    return <ErrorMessage message={error} />;
+  }
+
+  return (
+    <div className="grid">
+      {data?.categories.map((category) => (
+        <Link
+          key={category.idCategory}
+          to={`/category/${category.strCategory}`}
+        >
+          <div className="card">
+            <img
+              src={category.strCategoryThumb}
+              alt={category.strCategory}
+            />
+
+            <h2>{category.strCategory}</h2>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
