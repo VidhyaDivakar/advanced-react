8f8853e10d6255ea739573c69e562b1b92a2bfc1
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-13 00:35:45 -0400

    Updated the navbar

--- advanced-react/src/components/Navbar.tsx
+++ advanced-react/src/components/Navbar.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Navbar = () =>{
-    return <h1> Home</h1>
-}
+const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+ const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate(`/search?query=${search}`);
+    setSearch("");
+  };
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search recipes"
+        />
+      </form>
+
+      <Link to="/favorites">Favorites</Link>
+    </nav>
+  );
+};
 export default Navbar;
