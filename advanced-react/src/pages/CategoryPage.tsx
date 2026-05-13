c22a4eb98b65ff0727176b494651db1beb869f09
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-13 00:47:42 -0400

    Adding more files

--- advanced-react/src/pages/CategoryPage.tsx
+++ advanced-react/src/pages/CategoryPage.tsx
@@ -1,6 +1,38 @@
+import { useParams } from "react-router-dom";
+import RecipeCard from "../components/RecipeCard";
+import Spinner from "../components/Spinner";
+import ErrorMessage from "../components/ErrorMessage";
+import { useFetch } from "../hooks/useFetch";
+import type { Meal } from "../types/mealTypes";
+
+interface MealsResponse {
+  meals: Meal[];
+}
 
 const CategoryPage = () => {
-  return <h1>CategoryPage</h1>;
+  const { categoryName } = useParams();
+
+  const { data, loading, error } =
+    useFetch<MealsResponse>(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+    );
+
+  if (loading) return <Spinner />;
+
+  if (error) {
+    return <ErrorMessage message={error} />;
+  }
+
+  return (
+    <div className="grid">
+      {data?.meals.map((meal) => (
+        <RecipeCard
+          key={meal.idMeal}
+          recipe={meal}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CategoryPage;
--- advanced-react/src/pages/RecipeDetail.tsx
+++ advanced-react/src/pages/RecipeDetail.tsx
@@ -1,5 +1,71 @@
+import { useParams } from "react-router-dom";
 const RecipeDetail = () => {
-  return <h1>RecipeDetail</h1>;
+  const { recipeId } = useParams();
+
+  const { addFavorite, removeFavorite, isFavorite } =
+    useFavorites();
+
+  const { data, loading, error } =
+    useFetch<MealResponse>(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`
+    );
+
+  if (loading) return <Spinner />;
+
+  if (error) {
+    return <ErrorMessage message={error} />;
+  }
+
+  const meal = data?.meals[0];
+
+  if (!meal) return <h2>No Recipe Found</h2>;
+
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal[`strIngredient${i}`];
+    const measure = meal[`strMeasure${i}`];
+
+    if (ingredient) {
+      ingredients.push(`${measure} ${ingredient}`);
+    }
+  }
+
+  return (
+    <div>
+      <img
+        src={meal.strMealThumb}
+        alt={meal.strMeal}
+        width="300"
+      />
+
+      <h1>{meal.strMeal}</h1>
+
+      <button
+        onClick={() =>
+          isFavorite(recipeId!)
+            ? removeFavorite(recipeId!)
+            : addFavorite(recipeId!)
+        }
+      >
+        {isFavorite(recipeId!)
+          ? "Remove Favorite"
+          : "Add Favorite"}
+      </button>
+
+      <h2>Ingredients</h2>
+
+      <ul>
+        {ingredients.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <h2>Instructions</h2>
+
+      <p>{meal.strInstructions}</p>
+    </div>
+  );
 };
 
 export default RecipeDetail;
